cebd07e5db1dc7962ff5a42422b3ca8140de4f5a
Don Kuntz <don@dkuntz2.com>
2012-10-03 23:41:45 -0500

    init

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
+ln -s -f $(pwd)/vim/vim ~/.vim
+ln -s -f $(pwd)/vim/vimrc ~/.vimrc
+ln -s -f $(pwd)/bash/bash_colors ~/.bash_colors
+ln -s -f $(pwd)/bash/bashrc ~/.bashrc

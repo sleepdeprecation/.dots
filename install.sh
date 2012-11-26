497c9c16283e4924e2a7f288808d86eacad42d49
Don Kuntz <don@dkuntz2.com>
2012-11-25 19:48:30 -0600

    git config too

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ ln -s -f $(pwd)/vim/vim ~/.vim
 ln -s -f $(pwd)/vim/vimrc ~/.vimrc
 ln -s -f $(pwd)/bash/bash_colors ~/.bash_colors
 ln -s -f $(pwd)/bash/bashrc ~/.bashrc
+ln -s -f $(pwd)/git/gitconfig ~/.gitconfig
+

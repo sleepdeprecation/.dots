b940da9b37c2f6c92d7f11486188bc88b6fc16ee
Don Kuntz <don@dkuntz2.com>
2012-11-25 19:59:20 -0600

    updated install to reflect

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s -f $(pwd)/vim/vimrc ~/.vimrc
 ln -s -f $(pwd)/bash/bash_colors ~/.bash_colors
 ln -s -f $(pwd)/bash/bashrc ~/.bashrc
 ln -s -f $(pwd)/git/gitconfig ~/.gitconfig
+ln -s -f $(pwd)/gconf ~/.gconf
 

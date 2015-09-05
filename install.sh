ef5ef814cde16efcab1d99bb64a7108b147f14d6
Don Kuntz <don@kuntz.co>
2015-09-05 00:48:51 -0500

    Make the install script better

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
-# grab oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+DOTSTUFFDIR=$PWD
 
-git clone https://github.com/VundleVim/Vundle.vim.git $PWD/vim/vim/bundle/Vundle.vim
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+else
+    cd ~/.oh-my-zsh
+    git pull
+    cd $DOTSTUFFDIR
+fi
 
+if [ ! -d "$PWD/vim/vim/bundle/Vundle.vim" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $PWD/vim/vim/bundle/Vundle.vim
+else
+    cd $PWD/vim/vim/bundle/Vundle.vim
+    git pull
+    cd $DOTSTUFFDIR
+fi
+
+# overwrite anyways.
 ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
 ln -s -f $(pwd)/vim/vim ~/.vim
 ln -s -f $(pwd)/vim/vimrc ~/.vimrc
@@ -13,4 +27,7 @@ ln -s -f $(pwd)/git/gitconfig ~/.gitconfig
 ln -s -f $(pwd)/tmux.conf ~/.tmux.conf
 ln -s -f $(pwd)/zsh/dkuntz.zsh-theme ~/.oh-my-zsh/themes/dkuntz.zsh-theme
 
-chsh -s `which zsh`
+if [ $SHELL != `which zsh` ]; then
+    echo "chsh"
+    chsh -s `which zsh`
+fi

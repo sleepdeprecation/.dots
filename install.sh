2a39644a29a6906b0c004c6cb710dae60d2a5809
Don Kuntz <don@kuntz.co>
2016-04-27 14:40:40 -0500

    Move everything to top level because ytf not?

--- install.sh
+++ install.sh
@@ -9,15 +9,13 @@ else
 fi
 
 # overwrite anyways.
-ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
-ln -s -f $(pwd)/vim/vim ~/.vim
-ln -s -f $(pwd)/vim/vimrc ~/.vimrc
-ln -s -f $(pwd)/vim/gvimrc ~/.gvimrc
-ln -s -f $(pwd)/bash/bash_colors ~/.bash_colors
-ln -s -f $(pwd)/bash/bashrc ~/.bashrc
-ln -s -f $(pwd)/git/gitconfig ~/.gitconfig
+ln -s -f $(pwd)/zshrc ~/.zshrc
+ln -s -f $(pwd)/vim ~/.vim
+ln -s -f $(pwd)/vimrc ~/.vimrc
+ln -s -f $(pwd)/gvimrc ~/.gvimrc
+ln -s -f $(pwd)/gitconfig ~/.gitconfig
 ln -s -f $(pwd)/tmux.conf ~/.tmux.conf
-ln -s -f $(pwd)/zsh/dkuntz.zsh-theme ~/.oh-my-zsh/themes/dkuntz.zsh-theme
+ln -s -f $(pwd)/dkuntz.zsh-theme ~/.oh-my-zsh/themes/dkuntz.zsh-theme
 
 # install vim plugins
 vim +PlugClean +PlugInstall +qall

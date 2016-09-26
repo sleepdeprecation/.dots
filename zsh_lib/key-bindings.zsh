cb3442522d4eed62787e36d8ab0d75fe064a499d
Don Kuntz <don@kuntz.co>
2016-09-26 16:32:58 -0500

    Make arrow key search actuall work (also the delete key)

--- zsh_lib/key-bindings.zsh
+++ zsh_lib/key-bindings.zsh
@@ -4,11 +4,13 @@ bindkey '^r' history-incremental-search-backward
 
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
-bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
 
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
 
 bindkey ' ' magic-space
 bindkey '^[[1;5C' forward-word
+
+bindkey '^[[3~' delete-char

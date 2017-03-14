a927870acc7f2b62cce999d98594d790fb8a5032
Don Kuntz <dkuntz2@gmail.com>
2017-03-13 23:02:23 -0500

    zsh: venv function creates virtualenv if none exists

--- zsh_lib/virtualenv.zsh
+++ zsh_lib/virtualenv.zsh
@@ -2,11 +2,19 @@ function venv() {
   if [[ ! -z $1 ]]; then
     env_dir=$1
   else
-    if [[ ! -d "$PWD/env" ]]; then
-      echo "No virtualenv directory specified / \`env\` not found."
-      return
-    fi
     env_dir="env"
+  fi
+
+  if [[ ! -d "$PWD/env" ]]; then
+    echo "Creating new virtualenv in \`env\`."
+    echo -n "Which python should be used [python3]: "
+    read use_py
+
+    if [[ -z $use_py ]]; then
+      use_py="python3"
+    fi
+
+    virtualenv -p $use_py $env_dir
   fi
 
   source $PWD/env/bin/activate

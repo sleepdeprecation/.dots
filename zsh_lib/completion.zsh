925b0429c650cd586e6bbe5de35cbb867f68576d
Don Kuntz <dkuntz@journera.com>
2017-08-21 14:37:33 -0500

    zsh: change tab-completion matcher list formula
    The original one would cause extra characters to be added during
    completion. This no longer has that problem.

--- zsh_lib/completion.zsh
+++ zsh_lib/completion.zsh
@@ -7,5 +7,6 @@ setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
 
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|?=** r:|?=**'
+# zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|?=** r:|?=**'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''

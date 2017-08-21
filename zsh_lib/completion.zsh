de917b97a00535d2827953b8365136903a3de8b4
Don Kuntz <dkuntz@journera.com>
2017-08-21 14:38:29 -0500

    zsh: tweak completion settings

--- zsh_lib/completion.zsh
+++ zsh_lib/completion.zsh
@@ -1,11 +1,20 @@
 autoload -U compaudit compinit
 compinit
 
-unsetopt menu_complete
-unsetopt flowcontrol
-setopt auto_menu
+# menu_complete - auto insert first option on tab
+# auto_menu     - leave blank on first tab
+setopt menu_complete
+# setopt auto_menu
+
 setopt complete_in_word
 setopt always_to_end
+setopt complete_aliases
+
+# keep slash at end of autocomplete directory name
+setopt auto_param_slash
+
+setopt list_packed
+setopt list_rows_first
 
 # zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|?=** r:|?=**'
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'

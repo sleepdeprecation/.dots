8d982e6d366f32c86b33de53f9b463a71be6fc93
Don Kuntz <dkuntz@journera.com>
2018-04-05 11:48:05 -0500

    zsh: add ecr-login function, for great glory

--- zsh_lib/aliases.zsh
+++ zsh_lib/aliases.zsh
@@ -4,3 +4,15 @@ alias rezsh="source $HOME/.zshrc"
 title() {
   echo -ne "\033]0;"$*"\007"
 }
+
+ecr-login() {
+  if [[ ! -z $1 ]]; then
+    profile="$1"
+  elif [[ ! -z $AWS_PROFILE ]]; then
+    profile="$AWS_PROFILE"
+  else
+    profile=""
+  fi
+
+  $(env AWS_PROFILE="$profile" aws ecr get-login --no-include-email --region us-east-1)
+}

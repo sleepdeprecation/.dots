38ece2b66387e4df85dc6f2a08bcad00523573dc
Don Kuntz <dkuntz@journera.com>
2019-04-01 17:00:56 -0500

    alias: tf-ssh for quick ssh to an aws instance in terraform

--- zsh-lib/terraform.zsh
+++ zsh-lib/terraform.zsh
@@ -1,1 +1,6 @@
 alias tfvars="jq '[.modules[0].outputs | to_entries[] | .value = .value.value] | from_entries '"
+
+function tf-ssh() {
+    ip=$(terraform state show "aws_instance.$1" | awk '$1 == "public_ip" {print $3}')
+    ssh $ip
+}

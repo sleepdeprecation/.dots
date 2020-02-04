0cc84db6db8db7e4214384c779c5310b67788379
Don Kuntz <dkuntz@journera.com>
2020-02-04 16:26:40 -0600

    tf-ssh parse new output correctly

--- zsh-lib/terraform.zsh
+++ zsh-lib/terraform.zsh
@@ -1,6 +1,6 @@
 alias tfvars="jq '[.modules[0].outputs | to_entries[] | .value = .value.value] | from_entries '"
 
 function tf-ssh() {
-    ip=$(terraform state show "aws_instance.$1" | awk '$1 == "public_ip" {print $3}')
-    ssh $ip
+    ip=$(terraform state show -no-color "aws_instance.$1" | awk '$1 == "public_ip" {print $3}')
+    ssh ubuntu@$ip
 }

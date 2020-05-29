f38892c307383ef5893d782af597baec27a1abf1
Don Kuntz <dkuntz@journera.com>
2020-05-29 15:45:11 -0500

    correct tf-ssh

--- zsh-lib/terraform.zsh
+++ zsh-lib/terraform.zsh
@@ -1,6 +1,11 @@
 alias tfvars="jq '[.modules[0].outputs | to_entries[] | .value = .value.value] | from_entries '"
 
+function tf-ip() {
+  terraform state show -no-color "aws_instance.$1" | awk '$1 == "public_ip" {print substr($3, 2, length($3)-2)}'
+}
+
 function tf-ssh() {
-    ip=$(terraform state show -no-color "aws_instance.$1" | awk '$1 == "public_ip" {print $3}')
-    ssh ubuntu@$ip
+  # ip=$(terraform state show -no-color "aws_instance.$1" | awk '$1 == "public_ip" {print substr($3, 2, length($3)-2)}')
+  ip=$(tf-ip $1)
+  ssh ubuntu@"$ip"
 }
